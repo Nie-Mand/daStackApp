8a962cdb6634b85f2a1f492b68c083dfa2ce24ac
niemand <chill.out.x19@gmail.com>
2021-03-03 03:30:34 +0100

    env

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
       setError(false)
       setLoading(true)
       setButton('Fetching')
+      console.log(process.env.API, process.env.REACT_APP_API, process.env.REACT_API)
       await axios.get(`${process.env.API}/${input}`)
         .then(({ data }) => {
           if (!data) setError(true)
